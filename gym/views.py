589e66a7cd5b5ee6b4b77deb26945e667968539e
MariaFerrari0 <marialuisaferraridemorais@gmail.com>
2026-09-18 15:56:42 -0300

    ...

--- gym/views.py
+++ gym/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'gym/ home.html')
+    return render(request, 'gym/home.html', context={ 'name': 'Maria Luiza'})
 
 
 def contato(request):
