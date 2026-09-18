80bd218cd838c5b581b69b952b02b0035d3a28f6
MariaFerrari0 <marialuisaferraridemorais@gmail.com>
2026-09-18 20:25:27 -0300

    front

--- gym/urls.py
+++ gym/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from gym.views import home
+from . import views
 
-
-#dominio/g
 urlpatterns = [
-    
-    path('', home),
-   
+    path('', views.home, name='home'),
+
+    path('alunos/', views.lista_alunos, name='lista_alunos'),
+    path('alunos/cadastrar/', views.cadastrar_aluno, name='cadastrar_aluno'),
+
+    path('professores/', views.lista_professores, name='lista_professores'),
+    path('professores/cadastrar/', views.cadastrar_professor, name='cadastrar_professor'),
+
+    path('modalidades/', views.lista_modalidades, name='lista_modalidades'),
+    path('modalidades/cadastrar/', views.cadastrar_modalidade, name='cadastrar_modalidade'),
+
+    path('matriculas/', views.lista_matriculas, name='lista_matriculas'),
+    path('matriculas/cadastrar/', views.cadastrar_matricula, name='cadastrar_matricula'),
 ]
--- gym/views.py
+++ gym/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return render(request, 'gym/pages/home.html', context={ 'name': 'Maria Luiza'})
+    return render(request, 'gym/home.html')
 
-# Create your views here.
+
+def lista_alunos(request):
+    return render(request, 'gym/alunos/lista.html')
+
+
+def cadastrar_aluno(request):
+    return render(request, 'gym/alunos/cadastrar.html')
+
+
+def lista_professores(request):
+    return render(request, 'gym/professores/lista.html')
+
+
+def cadastrar_professor(request):
+    return render(request, 'gym/professores/cadastrar.html')
+
+
+def lista_modalidades(request):
+    return render(request, 'gym/modalidades/lista.html')
+
+
+def cadastrar_modalidade(request):
+    return render(request, 'gym/modalidades/cadastrar.html')
+
+
+def lista_matriculas(request):
+    return render(request, 'gym/matriculas/lista.html')
+
+
+def cadastrar_matricula(request):
+    return render(request, 'gym/matriculas/cadastrar.html')
