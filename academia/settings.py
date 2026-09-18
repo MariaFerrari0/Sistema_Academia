ce6c0ec20bdd5ef7e424243d3b6177c18b8542fa
MariaFerrari0 <marialuisaferraridemorais@gmail.com>
2026-09-18 17:30:02 -0300

    ..

--- academia/settings.py
+++ academia/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'academia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'gym' / 'templates', 
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
