0c755d266298fc45f1003252a76954d09d514846
MariaFerrari0 <marialuisaferraridemorais@gmail.com>
2026-09-19 02:24:20 -0300

    arrumando front

--- gym/static/gym/js/script.js
+++ gym/static/gym/js/script.js
@@ -1,7 +1,14 @@
 function toggleSidebar() {
-
     const sidebar = document.getElementById("sidebar");
+    const button = document.querySelector(".menu-button");
 
     sidebar.classList.toggle("active");
 
+    if (sidebar.classList.contains("active")) {
+        button.classList.remove("closed");
+    } else {
+        button.classList.add("closed");
+    }
 }
+
+
